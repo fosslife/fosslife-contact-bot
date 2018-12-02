ef3e67913660d907cbca0450ad0b809ab58e4c9e
Sparkenstein <zetabytes.pp@gmail.com>
2018-12-02 22:37:37 +0530

    destructured objects

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ bot.use(session())
 bot.start(({ reply }) => reply('Welcome to the fosslife. You can contact administrators with this bot and even share any posts/links you want to share'))
 bot.help(({ reply }) => reply('Type anything and send, the message will be automatically forwarded to admins'))
 
-bot.on('message', (ctx) => {
-  if (ctx.update.message.chat.id !== -1001168418071) {
-    ctx.forwardMessage(-1001168418071, ctx.update.message.from.id, ctx.update.message['message_id'])
-    return ctx.reply('Thanks for contacting')
+bot.on('message', ({forwardMessage, reply, update: {message}, tg: { sendMessage } }) => {
+  if (message.chat.id !== -1001168418071) {
+    forwardMessage(-1001168418071, message.from.id, message['message_id'])
+    return reply('Thanks for contacting')
   } else {
-    ctx.tg.sendMessage(ctx.update.message.reply_to_message.forward_from.id, ctx.update.message.text)
+    sendMessage(message.reply_to_message.forward_from.id, message.text)
   }
 })
 
